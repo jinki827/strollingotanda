bb06d4fa956a274c7626f93e842a6610a893446d
jinki827 <jinki.watanabe827@gmail.com>
2026-09-06 20:55:19 +0900

    historysectionのスライダー内の画像調整とsidemenuのJS変更

--- js/script.js
+++ js/script.js
@@ -1,30 +1,44 @@
 // マップにとぶボタンを押した時の関数
 function openMap(buttonElement) {
   // 押されたボタンからそれぞれのURLを取得
-  const googleUrl = buttonElement.getAttribute("data-google");
-  const appleUrl = buttonElement.getAttribute("data-apple");
+  const googleUrl = buttonElement.getAttribute("data-google")
+  const appleUrl = buttonElement.getAttribute("data-apple")
 
-  const isiPhone = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+  const isiPhone = /iPhone|iPad|iPod/i.test(navigator.userAgent)
 
   if (isiPhone) {
-    window.location.href = appleUrl;
+    window.location.href = appleUrl
   } else {
-    window.open(googleUrl, "_blank");
+    window.open(googleUrl, "_blank")
   }
 }
 
 // サイドメニューJS //
 document.getElementById("sideButton").addEventListener("click", function() {
-  this.classList.toggle("active");    // クラス追加
-  document.getElementById("sideBar").classList.toggle("active");
-  document.getElementById("mask").classList.toggle("active");
-});
+  this.classList.toggle("active")    // クラス追加
+  document.getElementById("sideBar").classList.toggle("active")
+  document.getElementById("mask").classList.toggle("active")
+})
+
+
+const menuLinks = document.querySelectorAll("#sideBar a")
+// querySelector : CSSの指定の仕方・要素を取得できる。※該当する最初の要素だけ取得
+// querySelectorAll：該当するすべての要素を取得する。node-listの形式で取得する。
+// node-list：複数の要素(node)
+
+console.log(location)
+const locationData = location
+menuLinks.forEach(link => {
+  if (link.getAttribute("href") === locationData.pathname) {
+    link.classList.add("active")
+  } 
+})
 
 
 // トップ画面のJS //
 
 // splide　メインビジュアル
-const mainVisualEl = document.querySelector("#mainVisual");
+const mainVisualEl = document.getElementById("mainVisual")
 if (mainVisualEl) {
   new Splide(mainVisualEl, {
     type: "fade",
@@ -35,19 +49,19 @@ if (mainVisualEl) {
     pauseOnHover: false,
     arrows: false,
     pagination: false, // 下部のドットナビゲーションを非表示する
-  }).mount();
+  }).mount()
 }
 
 // splide　historyセクション
-const historyMediaEl = document.querySelector("#historyMedia");
-const historyTextEl = document.querySelector("#historyText");
+const historyMediaEl = document.getElementById("historyMedia")
+const historyTextEl = document.getElementById("historyText")
 
 if (historyMediaEl && historyTextEl) {
   const historyMedia = new Splide(historyMediaEl, {
     speed: 800,
     arrows: false,
     pagination: true,
-  });
+  })
 
   const historyText = new Splide(historyTextEl, {
     type: "fade",
@@ -55,15 +69,15 @@ if (historyMediaEl && historyTextEl) {
     speed: 600,
     arrows: false,
     pagination: false,
-  });
+  })
 
-  historyMedia.sync(historyText);
-  historyMedia.mount();
-  historyText.mount();
+  historyMedia.sync(historyText)
+  historyMedia.mount()
+  historyText.mount()
 }
 
 // splide　galleryセクション
-const galleryMediaEl = document.querySelector("#galleryMedia");
+const galleryMediaEl = document.getElementById("galleryMedia")
 if (galleryMediaEl) {
   new Splide(galleryMediaEl, {
     type: "loop",
@@ -81,7 +95,7 @@ if (galleryMediaEl) {
     autoplay: true,
     speed: 1000,
     pagination: true,
-  }).mount();
+  }).mount()
 }
 
 
@@ -107,5 +121,5 @@ document.querySelectorAll(`.splideHistory`).forEach(slide => {
     speed: 1000,
     arrows: false,
     pagination: true,
-  }).mount();
-});
+  }).mount()
+})
