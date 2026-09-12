c7727cfd2afae5bc3a84bc243bec800300a17fb4
jinki827 <jinki.watanabe827@gmail.com>
2026-09-12 15:06:21 +0900

    fix:siteCoceptの部分だけfadeIn処理を早くすることで、画面高さによて処理が始まらないを対策した。

--- js/script.js
+++ js/script.js
@@ -183,12 +183,30 @@ const observer = new IntersectionObserver((entries, observer) => {
 })
 
 // `.fadeIn`など　動作をつけたいクラスを持つすべての要素を監視対象に登録する
-document.querySelectorAll(".fadeIn, .slideInLeft, .slideInRight, .charFadeIn").forEach(el => {
+document.querySelectorAll(".fadeIn, .slideInLeft, .slideInRight").forEach(el => {
   observer.observe(el)
 })
 
+// siteConceptのフェードイン処理用
+const observerCharFadeIn = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    // 対象の要素が画面内に入ったか判定
+    if (entry.isIntersecting) {
+      // 画面内に入ったら、`isActive` クラスを付与して、フェードイン
+      entry.target.classList.add("isActive")
 
+      // 初回だけ実行したいので、一度表示されたら要素の監視を解除する
+      observer.unobserve(entry.target)
+    }
+  })
+  }, {
+    // オプション設定
+    rootMargin: '0px 0px 0px 0px'  // 画面の下から100px 分入ったタイミングで発火させるときれいらしい
+})
 
+document.querySelectorAll(".charFadeIn").forEach(el => {
+  observerCharFadeIn.observe(el)
+})
 
 
 // historyPageのＪＳ //
